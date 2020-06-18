3fd815b84d5d4e12682411dcfe160ffe43e8a09f
romainlancia <romainlancia@gmail.com>
2020-06-19 00:36:07 +0200

    update pixel

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,8 +27,9 @@ import FloatingButton from '../components/FloatingButton'
 import { ExpandMore, ArrowUpward } from '@material-ui/icons'
 import ListEducation from '../components/List/ListEducation'
 import List from '../components/List'
-import { withFacebookPixel } from '../lib/hoc'
 import Text from '../components/Text'
+import Router from 'next/router'
+import withFBQ from 'next-fbq'
 
 function Page() {
     const [posScrollbar, setPosScrollbar] = useState(0)
@@ -46,93 +47,94 @@ function Page() {
                 <title>Romain Lancia</title>
             </Head>
             <Layout>
-            <Box
-                style={{
-                    display: 'flex',
-                    height: '100vh',
-                    maxHeight: '100vh',
-                    flexDirection: 'column',
-                    backgroundColor: BLUE_GREY_10_DARK,
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                }}
-            >
-                <Header />
-                <Text>Romain Lancia</Text>
-                <Text fontFamily="Economica" fontSize={32}>
-                    Software Engineer
-                </Text>
-                <Divider />
-                <FloatingButton
-                    goto={SECTION_TITLE_ABOUT}
-                    isVisible
+                <Box
                     style={{
-                        bottom: 40,
-                        alignSelf: 'center',
+                        display: 'flex',
+                        position: 'fixed',
+                        height: '100vh',
+                        maxHeight: '100vh',
+                        flexDirection: 'column',
+                        backgroundColor: BLUE_GREY_10_DARK,
+                        alignItems: 'center',
+                        justifyContent: 'center',
                     }}
                 >
-                    <ExpandMore />
-                </FloatingButton>
-            </Box>
-            <Section title={SECTION_TITLE_ABOUT}>
-                <Box>
+                    <Header />
+                    <Text>Romain Lancia</Text>
+                    <Text fontFamily="Economica" fontSize={32}>
+                        Software Engineer
+                    </Text>
+                    <Divider />
+                    <FloatingButton
+                        goto={SECTION_TITLE_ABOUT}
+                        isVisible
+                        style={{
+                            bottom: 40,
+                            alignSelf: 'center',
+                        }}
+                    >
+                        <ExpandMore />
+                    </FloatingButton>
+                </Box>
+                <Section title={SECTION_TITLE_ABOUT}>
+                    <Box>
+                        <Text
+                            color="black"
+                            textAlign="center"
+                            fontFamily="Roboto Condensed"
+                            fontSize={21}
+                            letterSpacing={1.75}
+                        >
+                            I am an enthusiastic Software Engineer. I like
+                            creating new user-friendly web and mobile
+                            applications. I am also very interested learning new
+                            things and get better and better each day.
+                        </Text>
+                    </Box>
+                </Section>
+                <Section title={SECTION_TITLE_SKILLS}>
+                    <List data={ALL_SKILLS} />
+                </Section>
+                <Section title={SECTION_TITLE_WORKS}>
+                    <ListWorks data={ALL_TASKS_WORKS} />
+                </Section>
+                <Section title={SECTION_TITLE_EDUCATION}>
+                    <ListEducation data={ALL_EDUCATION} />
+                </Section>
+                <Section title={SECTION_TITLE_LANGUAGES}>
+                    <List data={ALL_LANGUAGES} />
+                </Section>
+                <Section title={SECTION_TITLE_HOBBIES}>
                     <Text
                         color="black"
                         textAlign="center"
                         fontFamily="Roboto Condensed"
-                        fontSize={21}
+                        fontSize={14}
                         letterSpacing={1.75}
                     >
-                        I am an enthusiastic Software Engineer. I like creating
-                        new user-friendly web and mobile applications. I am also
-                        very interested learning new things and get better and
-                        better each day.
+                        Chess, self-development, start-up, technologies,
+                        reading, programming, piano
                     </Text>
-                </Box>
-            </Section>
-            <Section title={SECTION_TITLE_SKILLS}>
-                <List data={ALL_SKILLS} />
-            </Section>
-            <Section title={SECTION_TITLE_WORKS}>
-                <ListWorks data={ALL_TASKS_WORKS} />
-            </Section>
-            <Section title={SECTION_TITLE_EDUCATION}>
-                <ListEducation data={ALL_EDUCATION} />
-            </Section>
-            <Section title={SECTION_TITLE_LANGUAGES}>
-                <List data={ALL_LANGUAGES} />
-            </Section>
-            <Section title={SECTION_TITLE_HOBBIES}>
-                <Text
-                    color="black"
-                    textAlign="center"
-                    fontFamily="Roboto Condensed"
-                    fontSize={14}
-                    letterSpacing={1.75}
+                </Section>
+                <Section title={SECTION_TITLE_CONTACT} mb={8}>
+                    <List data={CONTACT_INFORMATIONS} />
+                </Section>
+                <FloatingButton
+                    goto={SECTION_TITLE_HOME}
+                    isVisible={posScrollbar > 200}
+                    style={{
+                        position: 'fixed',
+                        bottom: 40,
+                        right: 40,
+                        backgroundColor: '#d50000',
+                        color: 'white',
+                    }}
                 >
-                    Chess, self-development, start-up, technologies, reading,
-                    programming, piano
-                </Text>
-            </Section>
-            <Section title={SECTION_TITLE_CONTACT} mb={8}>
-                <List data={CONTACT_INFORMATIONS} />
-            </Section>
-            <FloatingButton
-                goto={SECTION_TITLE_HOME}
-                isVisible={posScrollbar > 200}
-                style={{
-                    position: 'fixed',
-                    bottom: 40,
-                    right: 40,
-                    backgroundColor: '#d50000',
-                    color: 'white',
-                }}
-            >
-                <ArrowUpward />
-            </FloatingButton>
+                    <ArrowUpward />
+                </FloatingButton>
             </Layout>
         </>
     )
 }
 
-export default withFacebookPixel(Page)
+export default withFBQ(process.env.FACEBOOK_PIXEL_ID, Router)(Page)
